6d69397f110a6a11a1510da126fb18f510192054
HoangDucBach <124645604+HoangDucBach@users.noreply.github.com>
2024-02-12 20:24:58 +0700

    Update object.controller.ts

---
+++ src/server/controllers/object.controller.ts
@@ -0,0 +1,10 @@
+export class TrendItem extend AObject {
+  private numberOfVotes: number;
+}
+export class Trend extend AObject {
+}
+export class User extend AObject {
+  private gmail:string;
+  private password: string;
+  
+}
