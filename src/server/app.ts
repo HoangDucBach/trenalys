b497dba71c523633d113edb7eee4883f1586b119
HoangDucBach <hoangbach0985@gmail.com>
2024-03-06 18:47:41 +0700

    deploy 6

--- src/server/app.ts
+++ src/server/app.ts
@@ -15,14 +15,24 @@ require('dotenv-flow').config({
 });
 
 const allowList = ['https://trenalys.vercel.app', 'https://trenalys.io.vn'];
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    next();
-});
-console.log(process.env.URL_CLIENT);
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", req.headers.origin);
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+//     next();
+// });
+const whitelist = ['https://trenalys.vercel.app', 'https://trenalys.io.vn']
+const corsOptions = {
+    origin: function (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
+        if (whitelist.indexOf(origin !== undefined ? origin : '') !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));
